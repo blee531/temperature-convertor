0e312e1a51c387b8c3ce2f834a48a9a315c602de
Ushio-Cheng <32724585+Ushio-Cheng@users.noreply.github.com>
2018-05-23 10:32:19 -0700

    Update UnitConvertor.m

--- UnitConvertor.m
+++ UnitConvertor.m
@@ -1,1 +1,16 @@
+//created by blee531 fork by ushio-cheng @ 20180523.100753
+//Project:Temperature Convertor
+//FileName:UnitConvertor.m
 
+#import "Fraction.h"
+
+@implementation UnitConvertor
+
+-(double)convertToCFromFtemperature:(double) input{
+
+}
+-(double)convertToFFromCtemperature:(double) input{
+
+}
+
+@end
